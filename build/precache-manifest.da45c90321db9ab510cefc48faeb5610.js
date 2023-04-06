a87123b00aad8cbd9fe43eb2a5bb67c591645707
Mirza Aleem Abbas <68433997+MirzaAleem@users.noreply.github.com>
2023-04-06 16:27:28 +0530

    Added Build Folder

---
+++ build/precache-manifest.da45c90321db9ab510cefc48faeb5610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25fac3cf9e5ebe5cc013",
+    "url": "/static/css/main.bba72d4c.chunk.css"
+  },
+  {
+    "revision": "25fac3cf9e5ebe5cc013",
+    "url": "/static/js/main.25fac3cf.chunk.js"
+  },
+  {
+    "revision": "7e3f84da8562feb845c1",
+    "url": "/static/js/1.7e3f84da.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99794846999fe53f5f958dcf25425a25",
+    "url": "/index.html"
+  }
+];
